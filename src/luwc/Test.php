92ac267b24b630112802bb276d4657e821d0951f
luweichen <luweichen@qq.com>
2022-05-26 16:53:32 +0800

    提交

---
+++ src/luwc/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace luwc;
+
+class Test
+{
+    public function test()
+    {
+        echo 'test 1';
+    }
+}
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+require 'vendor/autoload.php';
+
+$test   = new \luwc\Test();
+$test->test();
